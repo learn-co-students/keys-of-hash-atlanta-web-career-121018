c4d3ddb62bc472a6088a6065b38d44f092fe1bd2
github username <github email address>
2018-12-24 08:28:42 -0500

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,16 @@
+require'pry'
+
 class Hash
-  def keys_of(arguments)
-    # code goes here
-  end
+    def keys_of(*arguments)
+        hash_return = []
+        arguments.each do |args|
+            self.each do |hash_keys, hash_values|
+                if args == hash_values
+                    hash_return << hash_keys
+                end
+            end
+        end
+        hash_return
+    end
 end
+# binding.pry
